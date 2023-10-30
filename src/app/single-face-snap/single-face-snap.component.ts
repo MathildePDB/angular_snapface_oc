fa9829d32ad8a19afb5ba1de9c99c2db07094c2e
MathildePDB <114467024+MathildePDB@users.noreply.github.com>
2023-10-30 11:17:51 +0100

    Add files via upload

---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapService } from '../services/face-snaps.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
+})
+export class SingleFaceSnapComponent implements OnInit {
+  faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  constructor(
+    private faceSnapService: FaceSnapService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snap!';
+    const snapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapService.getFaceSnapById(snapId);
+  }
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+      this.buttonText = 'Oh Snap!'
+    }
+  }
+}
